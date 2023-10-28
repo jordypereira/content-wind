73850f46cc3ffa843a30c9b9f14ff9e1ee7a8617
Jordy <jordy.pereira@hotmail.com>
2023-10-28 15:42:39 +0200

    add content

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   // Configure Content Wind here
-  cover: '/cover.jpg',
+  cover: '/cover.png',
   socials: {
     twitter: '_jordypereira',
     github: 'jordypereira/content-wind',
